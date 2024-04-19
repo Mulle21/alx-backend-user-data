72581bcfc2535df4ecd1012df655b98a3bd51ca6
Mulle21 <mulukenab95@gmail.com>
2024-04-19 17:23:24 +0000

    task 0

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -70,15 +70,11 @@ def before_request() -> str:
     if not auth.require_auth(request.path, excluded_paths):
         return
 
-    if auth.authorization_header(request) is None \
-            and auth.session_cookie(request) is None:
+    if auth.authorization_header(request) is None:
         abort(401)
 
-    current_user = auth.current_user(request)
-    if current_user is None:
+    if auth.current_user(request) is None:
         abort(403)
-
-    request.current_user = current_user
 
 
 if __name__ == "__main__":
